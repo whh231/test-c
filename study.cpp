878e23470108ca06b1a9699e5b080d8bd0112755
whh231 <2043677384@qq.com>
2024-10-14 23:16:09 +0800

    Create study.cpp

---
+++ study.cpp
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main() {
+	int a, b;
+
+	scanf("%d %d", &a, &b);
+	if ((a%b) == 0)
+		printf("yes");
+	else {
+		printf("%d %d", (a / b), (a % b));
+
+	}
+
+	return 0;
+}
+
+
+
+
