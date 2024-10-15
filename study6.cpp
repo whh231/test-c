11772df91f29091753e58a4de675ed0994211900
whh231 <2043677384@qq.com>
2024-10-15 19:12:54 +0800

    Create study6.cpp

---
+++ study6.cpp
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main() {
+	int a,b,c;
+	scanf("%d %d %d",&a,& b,&c);
+	if (a*a+b*b==c*c) {
+		int s;
+		s = a * b ;
+		printf("%d",s);
+
+
+
+
+
+
+	}
+	else
+		printf("no");
+	return 0;
+}
+
+
+
+
